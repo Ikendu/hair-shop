d0e3529495c54120fc3d797677fa90fbb77a1921
Chibunud <ndubest56@gmail.com>
2023-11-16 07:14:35 +0100

    access control header

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
   })
 )
 app.all('/', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://hairview.onrender.com')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
   next()
 })
