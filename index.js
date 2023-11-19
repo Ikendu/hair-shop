5928ef23f66b4d806270bc478182b412061ebcc4
Chibunud <ndubest56@gmail.com>
2023-11-19 17:59:53 +0100

    uploading cors policy fixing

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bcrypt = require(`bcryptjs`)
 const jwt = require('jsonwebtoken')
 const cookieParser = require(`cookie-parser`)
 const multer = require('multer')
-const uploadMd = multer({ dest: `uploads/` })
 const fs = require(`fs`)
 const User = require('./models/User')
 const Post = require('./models/Post')
@@ -19,19 +18,17 @@ dotenv.config()
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://hairview.onrender.com'],
+    origin: ['http://localhost:5173', 'https://hairview.onrender.com', `uploads/`],
     credentials: true,
   })
 )
-var corsOptions = {
-  origin: ['http://localhost:5173', 'https://hairview.onrender.com'],
-  optionsSuccessStatus: 200,
-}
+
 app.all('/', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
   next()
 })
+const uploadMd = multer({ dest: `uploads/` })
 
 app.use(express.json())
 app.use(cookieParser())
@@ -88,7 +85,7 @@ app.post(`/logout`, (req, res) => {
   res.cookie(`token`, ``).json(`ok`)
 })
 
-app.post(`/create`, cors(corsOptions), uploadMd.single(`file`), async (req, res, next) => {
+app.post(`/create`, uploadMd.single(`file`), async (req, res, next) => {
   const { originalname, path } = req.file
   const div = originalname.split(`.`)
   const extension = div[div.length - 1].toLowerCase()
